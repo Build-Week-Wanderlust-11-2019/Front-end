3f150c1b9f93f38389ea569f36a186c6a1ea2f34
c00kamunga <tsothalanti2@yahoo.com>
2019-11-14 19:57:47 -0800

    created function validaitonInput and handleSubmit function

--- wonderlust/src/Components/Login.js
+++ wonderlust/src/Components/Login.js
@@ -7,4 +7,15 @@ import React from 'react';
 export default function Login(){
     const[username, setUserName] = useState("");
     const[password, setPassWord] = useState("");
+    const[checkbox, setCheckBox] = useState("");
 }
+
+
+function validationInput(){
+    return username.length > 0 && password.length > 0;
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+}
+
