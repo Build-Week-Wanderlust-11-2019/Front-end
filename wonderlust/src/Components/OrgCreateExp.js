5ee7998da262ed9291d87bbe504c71f81cf94e52
Roger Lee <rmlee0003@gmail.com>
2019-11-14 20:20:37 -0500

    created boiler plate Organizer home page and form component & navigation componentExperience

---
+++ wonderlust/src/Components/OrgCreateExp.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function OrgCreateExp(props) {
+
+ //state for form
+
+
+
+ // change handler
+
+
+ // submit handler
+
+ 
+ return (
+  <div>
+   {/*Form for creating experience needs inputs for experience_desc,date,experience_title,image*/}
+  </div>
+ );
+}
+
+export default OrgCreateExp;
--- wonderlust/src/Components/OrganizerHome.js
+++ wonderlust/src/Components/OrganizerHome.js
@@ -1,10 +1,29 @@
-import React from 'react';
-
+import React,{ useState, useEffect } from 'react';
+import api from '../Utils/AxiosAuth'
+import OrgCreateExp from './OrgCreateExp'
+import Experience from './Experience'
 function OrganizerHome(props) {
+
+
+//state for your orgs id
+//state for experiences []
+
+
+//get orgs experiences using orgId from backend API
+ useEffect(() => {
+  api().get('/api/exp:id')
+  .then(res => { 
+     //populate experiences []
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
+
+},[])
+
  return (
-  <div>
-   
-  </div>
+  <OrgCreateExp /> /*side form*/
+  /*map through experiences in state render Experience component for all exps <Experience key={index} data={experience} \> */
  );
 }
 
