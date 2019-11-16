74b3e1fd4b31087316ca26b7097c317b1635bfc9
c00kamunga <tsothalanti2@yahoo.com>
2019-11-15 19:09:50 -0800

    completed some of the navigation component

--- wonderlust/src/Components/Navigation.js
+++ wonderlust/src/Components/Navigation.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function Navigation(props) {
- return (
-  <div>
-   
-  </div>
- );
+    return (
+        <div>
+            <div className="Navbar">
+                <Link to="home"><div>Home</div></Link>
+                <Link to="About"><div>About</div></Link>
+                <Link to="Contact"><div>Contact</div></Link>
+            </div>
+        </div>
+    );
 }
 
 export default Navigation;
