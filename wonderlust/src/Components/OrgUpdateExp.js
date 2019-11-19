67430f9079c7c685a156d857c993cf5a46c7e10a
c00kamunga <tsothalanti2@yahoo.com>
2019-11-18 19:46:48 -0800

    added state for map, added state for orgupdate form

--- wonderlust/src/Components/OrgUpdateExp.js
+++ wonderlust/src/Components/OrgUpdateExp.js
@@ -8,29 +8,36 @@ import { connect } from 'react-redux'
 
 
 function OrgUpdateExp(props) {
+    const [searchTerm, setTerm] = useState("")
+    const key = '4ac18f2821024e6b8bb8e5643e56574a'
 
- //state for form
+    //state for form
+    const [updateExperience, setupdateExperience] = ({
 
 
 
- // change handler
 
 
- // submit handler
+    })
 
- 
- return (
-  <div>
-   <form style={style}>
+    // change handler
 
-   </form>
-  </div>
- );
+
+    // submit handler
+
+
+    return (
+        <div>
+            <form style={style}>
+
+            </form>
+        </div>
+    );
 }
 
 export default OrgUpdateExp;
 const style = {
- width:"45%",
- height:"100%",
- border:"2px solid black",
+    width: "45%",
+    height: "100%",
+    border: "2px solid black",
 }
