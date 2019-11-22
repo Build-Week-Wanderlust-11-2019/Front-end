8274ef311980be8219560270d895323b55456fa9
c00kamunga <tsothalanti2@yahoo.com>
2019-11-21 17:03:58 -0800

    created spinner component for loading pages

---
+++ wonderlust/src/LoadSpinner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+
+
+
+
+function LoadSpinner(){
+
+const [spin, setSpin] = useState(false)
+
+fetchData = () =>{
+    this.setState({ loading:true});
+}
+
+
+}
+
+export default LoadSpinner;
