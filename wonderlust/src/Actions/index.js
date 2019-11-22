dcc7b84aeb439d921e4bba76bc122ee4c288fa01
Roger Lee <rmlee0003@gmail.com>
2019-11-21 22:36:44 -0500

    added delete exps functionality

--- wonderlust/src/Actions/index.js
+++ wonderlust/src/Actions/index.js
@@ -1,4 +1,3 @@
-import { orgRegister, userRegister, orgLogin, userLogin } from "../Utils/LoginHelper";
 import api from '../Utils/AxiosAuth'
 export const ADDORGID = "ADDORGID"
 export const ISORG = "ISORG"
@@ -10,7 +9,7 @@ export const UPDATEEXPS = 'UPDATEEXPS'
 export const ADDUSER = "ADDUSER"
 export const SUCCESS = "SUCCESS"
 export const ADDINFO = "ADDINFO"
-
+export const DELETE = "DELETE"
 
 export function addInfo(name,id,isOrg){
  localStorage.setItem("id", id);
@@ -26,52 +25,21 @@ export function addInfo(name,id,isOrg){
  }
 }
 
-
-// export function loginUser(username,password,isOrg){
-//  return dispatch => {
-//   dispatch({ type: LOADING });
-//   if (isOrg) {
-//    orgLogin(username, password)
-//    .then(res => {
-//      dispatch({
-//        type: ADDUSER,
-//        payload: {
-//          name: username,
-//          isOrg: isOrg,
-//          orgId: res
-//        }
-//       });
-//     localStorage.setItem("id", res);
-//     localStorage.setItem("isOrg", isOrg);
-//     localStorage.setItem("name", username);
-     
-//    }).then( () => {
-    
-//     dispatch({ type: SUCCESS })
-//   })
-// }
-//  else {
-//   userLogin(username, password)
-//   .then(res => {
-//    dispatch({
-//     type: ADDUSER,
-//     payload: {
-//       name: username,
-//       isOrg: isOrg,
-//       orgId: null
-//     }
+export function deleteExp(userId,id){
+ console.log(userId)
+ return dispatch => {
+  api().delete(`/api/exp/${id}`)
+  .then(res => {
+   console.log(res)
+  api().get(`/api/exp/${userId}`) 
+ .then(res => {
+   console.log(res)
+   dispatch({ type:DELETE, payload:res.data})
+  })
+ })
   
- 
-//  })
-//  localStorage.setItem("id", null);
-//  localStorage.setItem("isOrg", isOrg);
-//  localStorage.setItem("name", username);
-// })
- 
-//  dispatch({ type: SUCCESS });
-// };
-// }}
- 
+ }
+}
 
 
 export function addExperience(experience){
